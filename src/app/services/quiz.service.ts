5d6a5c6e34271394a7727571c38afe616eefb1c8
pranjay-poddar <pranjaypoddar@outlook.com>
2021-08-20 21:28:29 +0530

    added self analysis

---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+
+  constructor(private http: HttpClient) { }
+  
+  get(url: string) {
+    return this.http.get(url);
+  }
+
+  getAll() {
+    return [
+      { id: 'assets/data/covid.json', name: 'Covid' },
+      { id: 'assets/data/mental.json', name: 'Mental' },
+      { id: 'assets/data/physical.json', name: 'Physical' },
+      { id: 'assets/data/respiratory.json', name: 'Respiratory' },
+      { id: 'assets/data/cholesterol.json', name: 'Cholesterol' }
+    ];
+  }
+}
