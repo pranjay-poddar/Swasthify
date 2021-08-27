480fe22d5aad2b2ade08af1be3679bb5e79cb44c
pranjay-poddar <pranjaypoddar@outlook.com>
2021-08-28 00:29:59 +0530

    corrected angular.json

--- src/app/Hospitals/dashboard/dashboard.component.ts
+++ src/app/Hospitals/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { flyInOut , expand} from '../../Utilities/animations/animation';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class DashboardComponent implements OnInit {
   
