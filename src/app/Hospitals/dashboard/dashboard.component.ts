16be07789de7889a4725ab5545f6a8024290b375
pranjay-poddar <pranjaypoddar@outlook.com>
2021-08-28 00:18:35 +0530

    patient dashboard updated

--- src/app/Hospitals/dashboard/dashboard.component.ts
+++ src/app/Hospitals/dashboard/dashboard.component.ts
@@ -10,29 +10,7 @@ import { flyInOut , expand} from '../../Utilities/animations/animation';
   ]
 })
 export class DashboardComponent implements OnInit {
-  sidenav = "";
-  sidenavTitle = "";
-  main_container = "main_container" 
-  toggleNav(){
-    if(this.sidenav){
-      this.sidenav = "";
-    }
-    else{
-      this.sidenav = "sidenav2";
-    }
-    if(this.sidenavTitle){
-      this.sidenavTitle = "";
-    }
-    else{
-      this.sidenavTitle = "sidenavTitle2";
-    }
-    if(this.main_container=="main_container"){
-      this.main_container = "main_container2";
-    }
-    else{
-      this.main_container = "main_container";
-    }
-  }
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/Patients/dashboard-patient/dashboard-patient.component.ts
+++ src/app/Patients/dashboard-patient/dashboard-patient.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { flyInOut , expand} from '../../Utilities/animations/animation';
 @Component({
   selector: 'app-dashboard-patient',
   templateUrl: './dashboard-patient.component.html',
-  styleUrls: ['./dashboard-patient.component.scss']
+  styleUrls: ['./dashboard-patient.component.scss'],
+  animations: [
+    flyInOut(),
+    expand()
+  ]
  
 })
 
@@ -11,7 +15,29 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class DashboardPatientComponent implements OnInit {
-  
+  sidenav = "";
+  sidenavTitle = "";
+  main_container = "main_container" 
+  toggleNav(){
+    if(this.sidenav){
+      this.sidenav = "";
+    }
+    else{
+      this.sidenav = "sidenav2";
+    }
+    if(this.sidenavTitle){
+      this.sidenavTitle = "";
+    }
+    else{
+      this.sidenavTitle = "sidenavTitle2";
+    }
+    if(this.main_container=="main_container"){
+      this.main_container = "main_container2";
+    }
+    else{
+      this.main_container = "main_container";
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
