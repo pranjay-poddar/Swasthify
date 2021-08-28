475abf902eaf892a8e46a01a2c0a24e414efab29
Nishant Singh <cool.nishant2705@gmail.com>
2021-08-29 01:59:24 +0530

    added animation

--- src/app/common-services/our-products/our-products.component.ts
+++ src/app/common-services/our-products/our-products.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { flyInOut, expand } from '../../Utilities/animations/animation';
 
 @Component({
   selector: 'app-our-products',
   templateUrl: './our-products.component.html',
-  styleUrls: ['./our-products.component.scss']
+  styleUrls: ['./our-products.component.scss'],
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class OurProductsComponent implements OnInit {
 
