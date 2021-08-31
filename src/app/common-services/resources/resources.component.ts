1564c13bec26f54f829580c267fb84ead1f70a9d
Nishant Singh <cool.nishant2705@gmail.com>
2021-08-31 18:48:31 +0530

    added animations

--- src/app/common-services/resources/resources.component.ts
+++ src/app/common-services/resources/resources.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { flyInOut, expand } from '../../Utilities/animations/animation';
 
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
-  styleUrls: ['./resources.component.scss']
+  styleUrls: ['./resources.component.scss'],
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class ResourcesComponent implements OnInit {
 
