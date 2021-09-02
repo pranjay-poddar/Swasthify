8e54878c037336f08d82458cc957f03206f0821b
Nishant Singh <cool.nishant2705@gmail.com>
2021-09-02 16:43:40 +0530

    added hospital-dashboard

--- src/app/Hospitals/dashboard/dashboard.component.ts
+++ src/app/Hospitals/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Hospitals } from 'src/app/models/hospitals';
 import { HospitalService } from 'src/app/services/hospital.service';
 import Swal from 'sweetalert2';
 
-import { flyInOut , expand} from '../../Utilities/animations/animation';
+import { flyInOut, expand } from '../../Utilities/animations/animation';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -15,10 +16,10 @@ import { flyInOut , expand} from '../../Utilities/animations/animation';
   ]
 })
 export class DashboardComponent implements OnInit {
-
-  id ! : number;
-  hospitals : Hospitals = new Hospitals();
-  constructor(private hospService : HospitalService, private router : ActivatedRoute) { }
+  step: any = 1;
+  id !: number;
+  hospitals: Hospitals = new Hospitals();
+  constructor(private hospService: HospitalService, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
     // this.id = this.router.snapshot.params['id'];
@@ -29,8 +30,10 @@ export class DashboardComponent implements OnInit {
     // (Error) => {
     //   console.log(Error.error.message);
     // });
+    this.hospitals.hospitalName = "Max Hospital"
+    this.hospitals.emailId = "max@gmail.com"
   }
-  updateEntries(){
+  updateEntries() {
     // this.hospService.updateDetailsOfHospital(this.id, this.hospitals).subscribe((data) => {
     //   this.hospitals = data;
     //   console.log(this.hospitals); 
@@ -39,6 +42,13 @@ export class DashboardComponent implements OnInit {
     // (Error) => {
     //   console.log(Error.error.message);
     // })
+  }
+
+  submit() {
+    this.step = this.step + 1;
+  }
+  previous() {
+    this.step = this.step - 1;
   }
 
 }
